b8b7ac753b457494c881f02bef6621d9285b22fd
putragabrielll <putragmahk@gmail.com>
2023-10-24 14:22:21 +0700

    first commit

---
+++ tugas1.js
@@ -0,0 +1,41 @@
+const biodata = {
+    nama : 'Gabriel putra',
+    age : 23,
+    hobbies : ['Travelling', 'Watch', 'Ngoding'],
+    isMarried : false,
+    schoolList : [
+        {
+            nameSchool : 'SMA Advent Purwodadi',
+            yearIn : 2015,
+            yearOut : 2018,
+            major : 'IPS'
+        },
+        {
+            nameSchool : 'Universitas Advent Indonesia',
+            yearIn : 2018,
+            yearOut : 2022,
+            major : 'TI'
+        }
+    ],
+    skills : [
+        {
+            skillName : 'HTML',
+            level : 'good'
+        },
+        {
+            skillName : 'CSS',
+            level : 'good'
+        },
+        {
+            skillName : 'Js',
+            level : 'beginner'
+        },
+        {
+            skillName : 'mySQL',
+            level : 'beginner'
+        }
+    ],
+    interestInCoding : true
+}
+
+console.log(biodata)
