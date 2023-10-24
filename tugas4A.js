139d1a50c6cb8132647150657e7a87299ff12da0
putragabrielll <putragmahk@gmail.com>
2023-10-24 16:58:47 +0700

    tugas4A

---
+++ tugas4A.js
@@ -0,0 +1,18 @@
+let data = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+  },
+  phone: "1-770-736-8031 x56442",
+  website: "hildegard.org",
+};
+
+let data_baru = { ...data, name: "Gabriel", email: "puragmahk@gmail.com", hobby: "Ngoding" };
+
+console.log(data_baru)
